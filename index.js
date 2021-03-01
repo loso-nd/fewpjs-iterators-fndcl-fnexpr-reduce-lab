da10a949584be51b3f73845ce80f98d73b66d5be
Nahum <losond8@gmail.com>
2021-03-01 16:40:06 -0500

    The lab was tricky, used notes from Using the Array Reduce Method Review Section

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const reducer = function (batteryBatches, reducer, init){
+//     let totalBatteries = init; 
+//     for (const el of batteryBatches){
+//         accum = reducer(acucum, element)
+//     }
+
+//     return accum;
+// }
+
+// function batteryCount(totalAmount, location){
+//     return totalAmount + location.ammount
+// }
+
+const totalBatteries = batteryBatches.reduce(function(accumulator, element){ return element + accumulator}, 0)
